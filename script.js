8f071300b40544c04a512431da82c800a2e319b6
jakejonggubaek <jakejonggubaek@gmail.com>
2021-02-20 19:32:33 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+/**
+ *
+ * I don't know what to cook!
+ *
+ * app heading.
+ * " I don't know what to cook!"
+ * 
+ * namespace
+ *
+ * input form where the user can add ingredients that they have.
+ *
+ * show the ingredients list below the form.
+ *
+ * delete button beside of the ingredients list so user can edit.
+ *
+ * listen to a button click event to generate the recipe.
+ * :get ingredients input value & generate random menu -> send them to the API
+ *
+ * receive recipe data from API -> display the recipe with ingredients that user might have to buy
+ *
+ * a button where user can see different option (recipe)
+ *
+ */
+
+
+//namespace
+const recipeApp = {};
+
+//save relevant API information
+recipeApp.apiUrl = 'https://api.edamam.com/search'; 
+recipeApp.apiKey = '293c08c7945e511e5cb756b37b0c5179';
+
+//create a method which requests informtion from the API
+recipeApp.getRecipe = () => {
+
+    //use URL constructor to specify the parameters we wish to include in our API endpoint
+    const url = new URL(recipeApp.apiUrl);
+    url.search = new URLSearchParams({
+        q: 'carrot, onions',
+        app_id: 'e941670e',
+        app_key: recipeApp.apiKey
+    });
+
+    fetch(url)
+        .then((response) => {
+            //parse this response into JSON and return
+            return response.json();
+        })
+        //parse the JSON promise response and log out readable data
+        .then((jsonResponse) => {
+            
+            const randomNumber = Math.floor(Math.random()*10);
+            jsonResponse.hits[randomNumber].recipe.label;
+            console.log(jsonResponse.hits[randomNumber].recipe.label);
+            //pass the data into the displayPhotos method
+        });
+};
+
+recipeApp.displayRecipe = () => {
+    const recipeName = document.querySelector('h2');
+    const ingredientList = document.querySelector('ul');
+};
+
+recipeApp.init = () => {
+    recipeApp.getRecipe();
+};
+
+
+
+recipeApp.init();
